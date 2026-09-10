f048d9a369e411f7347b8884bfff828011dc40c8
Darkhan Kubigenov <darkhanu@gmail.com>
2026-09-10 11:00:44 +0100

    Add theme-init script

---
+++ theme-init.js
@@ -0,0 +1,6 @@
+(function () {
+  var theme = localStorage.getItem("theme");
+  if (theme === "light" || theme === "dark") {
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+})();
